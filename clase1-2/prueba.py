f89ea8c83f5f7c8cf0a19e111a10151230b8e6d8
Benjamin F Vales <benja.vales@gmail.com>
2025-09-08 12:51:46 -0300

    cambios estudios 8/9

--- clase1-2/prueba.py
+++ clase1-2/prueba.py
@@ -1,24 +1,35 @@
-archivo = "ejemplo.txt"
-#with open(archivo, 'rt') as file:
-#    data =file.read()
-#    data
-#    print(data)
-    
+#Tengo que escribir la ruta del archivo en cada celda
+archivo = "/home/jusa/Escritorio/LabodeDatos/clase1-2/ejemplo.txt" #asi funciona bien
 
+#%%
+#manera automatica
+#archivo = 'ejemplo.txt' #asi no funciono una vez que probe
+archivo = "/home/jusa/Escritorio/LabodeDatos/clase1-2/ejemplo.txt" #asi funciona bien
+with open(archivo, 'rt') as file:
+    data =file.read()
+    data
+    print(data)
+#%%
+    
+#%%
+#Manera manual
+archivo = "/home/jusa/Escritorio/LabodeDatos/clase1-2/ejemplo.txt" #asi funciona bien
 with open(archivo,'rt') as file:
     data = file.read()
-#f = open(archivo, 'rt')
-#data = f.read()
-#f.close()
-#data
-#print(data)
+f = open(archivo, 'rt')
+data = f.read()
+f.close()
+data
+print(data)
+#%%
 
+#%%
 #with open(archivo,'rt') as file:
-    data = file.read()
-#        data_nuevo = 'inicio del texto' + data
-#        data_nuevo = data _nuevo + 'cierre del texto'
+#    data = file.read()
+#    data_nuevo = 'inicio del texto' + data
+#    data_nuevo = data _nuevo + 'cierre del texto'
 #    
-#        datame = open("nuevonombre.txt",'w') #write mode
-#       datame.write (data_nuevo)
-#   datame.close()
-    
+#    datame = open("nuevonombre.txt",'w') #write mode
+#    datame.write (data_nuevo)
+#    datame.close()
+#%%    
