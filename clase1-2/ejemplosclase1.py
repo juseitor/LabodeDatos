2b7bbd8c29b6134d7fad25d5c34921eb8d7879a8
Benjamin F Vales <benja.vales@gmail.com>
2025-09-09 21:29:23 -0300

    cambio de lugar archivos y avanzo el la clase

---
+++ clase1-2/ejemplosclase1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+#%%
+import random
+prueba = random.random()
+print(prueba)
+
+import csv
+#%%
+
+a = np.array([1,2,3,4,5,6]) #cada lista es una columna
+b = np.array ([[1,2,3,4],[5,6,7,8],[9,10,11,12]]) #dos dimensiones
+print(a[0])
+print(b[0]) #por default es fila
+print(b[2][3]) #fila x columna
+print(b[2,3])
+np.zeros(2) #matriz de ceros del tamaño indicado
+np.zeros((2,3))
+#%%
+import numpy as np
+
+np.arange(4) # array([0,1,2,3])
+
+#%%
+import pandas as pd
+
+fname = '/home/Estudiante/Escritorio/LabodeDatos/Clase3/cronograma_sugerido.csv'
+df = pd.read_csv(fname)
+#%%
+
