ad53608feafc8d9fdbbf8ca8f23c14842342703b
Benjamin F Vales <benja.vales@gmail.com>
2025-10-02 15:49:31 -0300

    agrogo progreso final clase12-13/

--- clase12-13/clase12-13.py
+++ clase12-13/clase12-13.py
@@ -14,6 +14,7 @@ graficoco2= df_2022.plot(
         y = "co2",
         kind ="line",
         title = "Emisiones de CO2 en 2022", 
+#        logy=True
         )
 graficoco2.set_yscale("log")
 
@@ -52,9 +53,10 @@ df_oil = agrupa_otros(
     df_2022, "oil_co2", umbral=0.03
 ).copy()  # el nombre agrupado queda en "otrosName"
 
-graficoCO2 = df_2022.plot(
-        x = "Name",
+graficoCO2 = df_oil.plot(
+        x = "otrosName",
         y = "oil_co2",
+        kind = "bar",
         title = "Emisiones de CO2 por Paises mas grandes en 2022", 
         legend = True,
         grid = True,
@@ -63,17 +65,54 @@ graficoCO2 = df_2022.plot(
 
 #%%
 
-#6 Hacer dos gráficos distintos que muestre los países que más CO2 emiten por consumo de petróleo
+#6.1 Repetir lo anterior (con el tipo de gráfico que les parezca mejor) para gas y carbón (coal)
 
-graficoCO2x = df_2022.plot(
-        x = "oil_co2",
-        y = "co2",
-        title = "Emisiones de CO2 por Consumo por petroleo en 2022", 
-        legend = True,
+df_coal = agrupa_otros(df_2022,"coal_co2",umbral=0.03).copy()
+
+graficoCOAL_CO2 = df_coal.plot(
+        y = "coal_co2",
+        labels=df_coal["otrosName"],
+        kind = "pie",
+        title = "Emisiones de CO2 por Consumo de Carbon en 2022", 
+        legend = False,
         grid = True,
         color = "red",
         )
+#df_oil.plot(kind="pie", y="oil_co2", labels=df_oil["otrosName"], legend=False)
+
+#%%
+
+df_gas = agrupa_otros(df_2022,"gas_co2",umbral=0.03).copy()
+
+graficoGAS_CO2 = df_gas.plot(
+        x = "otrosName",
+        y = "gas_co2",
+        kind = "bar",
+        title = "Emisiones de CO2 por Consumo de Gas en 2022",
+        grid = True,
+        color = "red",
+    )
 
 
+#%%
+
+#VISUALIZACIONES_2
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%%
+
+fname1= "~/LabodeDatos/clase12-13/archivosclase12-13/imdb-dataset.csv"
+
+df = pd.read_csv(fname1)
+
+#%%
+
+df["quintiles_duracion"] = pd.qcut(
+    df["Duration"], 5, labels=[1, 2, 3, 4, 5]
+)  # se le pueden poner otros nombres
 
 #%%
