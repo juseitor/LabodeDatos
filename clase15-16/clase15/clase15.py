b568977e64a6c8785103528299728a6a825e9662
Benjamin F Vales <benja.vales@gmail.com>
2025-10-30 19:36:57 -0300

    avanzo con clase 15

--- clase15-16/clase15/clase15.py
+++ clase15-16/clase15/clase15.py
@@ -5,14 +5,14 @@ from sklearn.neighbors import KNeighborsClassifier
 
 #%%
 
-df_diabetes = pd.read_csv("~/LabodeDatos/clase15-16/archivosclase15-16/diabetes.csv")
+df_diabetes = pd.read_csv("~/LabodeDatos/clase15-16/clase15/archivosclase15/diabetes.csv")
 
 #%% X atributos, y etiqueta
 
 X = df_diabetes[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]
 y = df_diabetes['Outcome']
 
-#%% para usar solo 2 atributos
+#%% para usar solo los 2 atributos glucosa y bmi
 
 X2 = df_diabetes[['Glucose', 'BMI']].values
 y = df_diabetes['Outcome'].values
