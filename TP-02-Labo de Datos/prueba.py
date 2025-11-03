b48f88eece2fa17404175350a736ef7df0afe338
Benjamin F Vales <benja.vales@gmail.com>
2025-11-03 20:31:38 -0300

    agrego tp2

--- TP-02-Labo de Datos/prueba.py
+++ TP-02-Labo de Datos/prueba.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+ 
 #%%
 
 df_minst = pd.read_csv('~/LabodeDatos/TP-02-Labo de Datos/TablasOriginales/kmnist_classmap_char.csv')
